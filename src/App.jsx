178c76dec5e78a3ef5866f0d06eaf19cd5d8124f
Jesús Hernández <jesushdez1303@gmail.com>
2024-11-09 13:20:04 -0400

    states_im_tired

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useState} from "react";
+
+
+
+
+
 function App() {
+  const nombre = "Jesús";
+  const [num, setNum] = useState(1);
+  
+  const sumar = () => {
+    setNum(num + 1)
+  }; 
+
+  const [input, setInput] = useState("");
+
+  const handleInput = (Event) =>{
+    const value = Event.target.value;
+    setInput(value);
+  }
+
+
   return (
       <div className="App">
-        <h1>hello world</h1>
+        <h1>hello {nombre}</h1>
+        <button onClick={sumar}>Press here to add 1</button>
+        <p>{num}</p>
+
+        <input onChange={handleInput}/>
+        <p>{input}</p>
       </div>
   )
 }
